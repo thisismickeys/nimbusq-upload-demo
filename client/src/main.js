69ef425afae6945b8f79290afb9c39022ed8872e
Mickey Sanchez <thisismickeyss@gmail.com>
2025-06-10 15:46:23 -0400

    save

--- client/src/main.js
+++ client/src/main.js
@@ -1,2 +1,4 @@
 import './firebase.js';
 import './contentModeration.js';
+formData.append('userTier', document.getElementById('userTier').value);
+formData.append('licenseeId', 'demo'); // or get from somewhere
